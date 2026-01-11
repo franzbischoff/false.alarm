436decd620ff93cf9bf5b51de86cf3f17dfc76fd
Francisco Bischoff <franzbischoff@gmail.com>
2026-01-11 20:05:47 +0000

    Add new parameter analysis output files for various metrics and interactions
    Signed-off-by: Francisco Bischoff <franzbischoff@gmail.com>

--- scripts/regime_detection/evaluation/parameter_analysis.R
+++ scripts/regime_detection/evaluation/parameter_analysis.R
@@ -13,8 +13,8 @@
 # CONFIGURATION
 # =============================================================================
 # nolint start
-DATASET <- "malignantventricular" # Options: malignantventricular, afib_regimes, vtachyarrhythmias
-METRIC <- "f3_weighted" # Options: f1_classic, f1_weighted, f3_classic, f3_weighted,
+DATASET <- "afib_regimes" # Options: malignantventricular, afib_regimes, vtachyarrhythmias
+METRIC <- "nab_score_standard" # Options: f1_classic, f1_weighted, f3_classic, f3_weighted,
 #          recall_4s, recall_10s, precision_4s, precision_10s,
 #          edd_median_s, fp_per_min,
 #          nab_score_standard, nab_score_low_fp, nab_score_low_fn
